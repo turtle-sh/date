5b0636b988268f697267a306dfb801e3a3318118
Joe Sullivan <its.joe.sullivan@gmail.com>
2013-10-02 19:39:59 -0400

    initial test

--- index.js
+++ index.js
@@ -28,5 +28,21 @@ define({
   },
   
   // "date -v"
-  "version": "0.1.1"
+  "version": "0.1.1",
+  
+  // Very early idea of what this might look like
+  "test": function() {
+    var val;
+    var obj = {
+      stdout: {
+        write: function() { val = arguments[0]; }
+      },
+      exit: function() {};
+    };
+    var args = this.parse('date -r 0');
+    this.fn.apply(obj, this.parse('date -r 0') );
+    if(val !== new Date(0) ) {
+      throw "date -r 0 should equal new Date(0)";
+    }
+  }
 });
