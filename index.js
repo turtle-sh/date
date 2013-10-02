50966503eb69d3285fbbf24f27197dc48211bf1d
Joe Sullivan <its.joe.sullivan@gmail.com>
2013-10-02 19:12:50 -0400

    Re-wrote with a more friendly api

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+// This is just a require.js module
 define({
-  "expr": /^date/,
-  "fn": function(argv) {
-    stdout = this.stdout;
-    exit = this.exit;
-    argv.unshift('node');
-    var program = new Commander();
-    try {
-    program
-      .option('-r, --represent [seconds]', 'Seconds since epoch date', parseInt)
-      .parse(argv);
-    } catch(e) {
-      return;
-    }
-    var date;
-    if(typeof program.represent === 'number') {
-      date = new Date(program.represent);
-    } else {
-      date = new Date()
-    }
-    this.stdout.log(date.toString());
+  
+  // The command-line name
+  "name": "date",
+  
+  // Any aliases
+  "aliases": [],
+  
+  // The actual function
+  "fn": function() {
+    // Think of "this" as "process"
+    this.stdout.write(new Date());
+    // Return
     this.exit();
-  }
+  },
+  
+  // One-liner.
+  "tagLine": "display date and time",
+  
+  // ...
+  "description": "The __date__ utility displays the current date and time.",
+  
+  // Middleware for parsing command-line options
+  "parse": function(argv) {
+  },
+  "version": "0.1.1"
 });
